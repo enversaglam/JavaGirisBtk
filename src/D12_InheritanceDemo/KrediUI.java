feb2b450638db1225009677724d3fbaff5aba608
enversaglam <enversaglam@gmail.com>
2024-05-02 02:30:00 +0200

    E Btk 12 son

--- src/D12_InheritanceDemo/KrediUI.java
+++ src/D12_InheritanceDemo/KrediUI.java
@@ -1,7 +1,7 @@
 package D12_InheritanceDemo;
 
 public class KrediUI {
-    public void KrediHesapla (OgretmenKrediManager ogretmenKrediManager){
-        ogretmenKrediManager.Hesapla();
+    public void KrediHesapla (BaseKrediManager baseKrediManager){
+        baseKrediManager.Hesapla();
     }
 }
--- src/D12_InheritanceDemo/Main.java
+++ src/D12_InheritanceDemo/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //OgretmenKrediManager ogretmenKrediManager = new OgretmenKrediManager();
         //ogretmenKrediManager.Hesapla();
-        //KrediUI krediUI = new KrediUI();
-        //krediUI.KrediHesapla(new OgretmenKrediManager());
+        KrediUI krediUI = new KrediUI();
+        krediUI.KrediHesapla(new OgretmenKrediManager());
+        krediUI.KrediHesapla(new TarimKrediManager());
 
     }
 }
