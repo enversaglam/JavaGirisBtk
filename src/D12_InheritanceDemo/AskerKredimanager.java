807de0bffba5d51283b8478227dff312a7bc5a73
enversaglam <enversaglam@gmail.com>
2024-05-03 19:22:33 +0200

    Pc Btk 12 Class

---
+++ src/D12_InheritanceDemo/AskerKredimanager.java
@@ -0,0 +1,5 @@
+package D12_InheritanceDemo;
+
+public class AskerKredimanager extends BaseKrediManager{
+    
+}
--- src/D12_InheritanceDemo/Main.java
+++ src/D12_InheritanceDemo/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         //OgretmenKrediManager ogretmenKrediManager = new OgretmenKrediManager();
         //ogretmenKrediManager.Hesapla();
+        //Polymorphism
         KrediUI krediUI = new KrediUI();
         krediUI.KrediHesapla(new OgretmenKrediManager());
         krediUI.KrediHesapla(new TarimKrediManager());
+        krediUI.KrediHesapla(new AskerKredimanager());
 
     }
 }
